d351ea5fc3c3d0ecdc2438aa2af7954c95d93ee8
tturdumamatovv <muki2022@mail.ru>
2024-01-01 18:19:24 +0600

    imported models and engine from db folder, craeted engine

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from db import models
+from db.database import engine
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return "Hello World!"
+
+
+models.Base.metadata.create_all(engine)
