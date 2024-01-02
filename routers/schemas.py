86d6fb2fd2537801a8f2ca9bd2650d47ee46d88e
tturdumamatovv <muki2022@mail.ru>
2024-01-02 21:37:10 +0600

    made schemas for postcreate and how to display that

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class UserBase(BaseModel):
@@ -10,6 +11,26 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+
+    class Config:
+        orm_mode = True
+        exclude = ["email", "password"]
+
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: UserDisplay
 
     class Config:
         orm_mode = True
