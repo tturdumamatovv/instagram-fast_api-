31a9edc85480aa9c3526814b047bd45700e4f295
tturdumamatovv <muki2022@mail.ru>
2024-01-03 19:55:15 +0600

    include authentication router

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from fastapi.staticfiles import StaticFiles
 from db import models
 from db.database import engine
 from routers import user, post
+from auth import authentication
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(authentication.router)
 
 
 @app.get("/")
