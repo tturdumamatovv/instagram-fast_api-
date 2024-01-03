a8b5d76ce9b9b3f29b8a0e409cbe670dc3d7b73f
tturdumamatovv <muki2022@mail.ru>
2024-01-03 19:53:13 +0600

    made function to get user by username

--- db/db_user.py
+++ db/db_user.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm.session import Session
 
+from fastapi import HTTPException, status
+
 from routers.schemas import UserBase
 from db.models import DbUser
 from db.hashing import Hash
@@ -15,3 +17,11 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_user_by_username(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with username {username} not found.")
+    return user
