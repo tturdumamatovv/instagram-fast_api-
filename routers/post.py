862dab9f6ce67f3abc329afeaf900abfabb0f2d1
tturdumamatovv <muki2022@mail.ru>
2024-01-03 19:54:15 +0600

    made permission to create post and upload images(user have to login first)

--- routers/post.py
+++ routers/post.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, status, UploadFile, File
 from fastapi.exceptions import HTTPException
 
-from routers.schemas import PostBase, PostDisplay
+from auth.oauth2 import get_current_user
+from routers.schemas import PostBase, PostDisplay, UserAuth
 from db.database import get_db
 from db import db_post
 
@@ -21,7 +22,7 @@ image_url_types = ['absolute', 'relative']
 
 
 @router.post('', response_model=PostDisplay)
-def create(request: PostBase, db: Session = Depends(get_db)):
+def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                             detail="Parameter image_url_type can only take 'absolute' or 'relative'.")
@@ -34,7 +35,7 @@ def posts(db: Session = Depends(get_db)):
 
 
 @router.post('/images')
-def upload_image(image: UploadFile = File(...)):
+def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
     letters = string.ascii_letters
     rand_str = ''.join(random.choice(letters) for i in range(6))
     new = f'_{rand_str}.'
