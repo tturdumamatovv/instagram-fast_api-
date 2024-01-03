f79275bc2316f00db0e4e0e54c5fb364bf1239bc
tturdumamatovv <muki2022@mail.ru>
2024-01-03 20:35:18 +0600

    made schema for comments

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from typing import List
+
 
 class UserBase(BaseModel):
     username: str
@@ -30,6 +32,16 @@ class UserDisplayForPosts(BaseModel):
         orm_mode = True
 
 
+# For PostDisplay
+class Comment(BaseModel):
+    text: str
+    username: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
+
+
 class PostDisplay(BaseModel):
     id: int
     image_url: str
@@ -37,6 +49,7 @@ class PostDisplay(BaseModel):
     caption: str
     timestamp: datetime
     user: UserDisplayForPosts
+    comments: List[Comment]
 
     class Config:
         orm_mode = True
@@ -46,3 +59,9 @@ class UserAuth(BaseModel):
     id: int
     username: str
     email: str
+
+
+class CommentBase(BaseModel):
+    username: str
+    text: str
+    post_id: int
